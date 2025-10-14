afb55be9fdd35cbfdead63e188a9dcfe5a91c8ae
Lana-DDOS <lana-ddos@LAPTOP-1OIR2NLU.lan>
2025-10-14 16:40:57 +0200

    test shit

---
+++ ETL/load.py
@@ -0,0 +1,30 @@
+# from logGenerator import log_generator
+# from dotenv import load_dotenv
+# import os
+# import logging
+# import sqlite3
+# from SQLite_database_connector import SQLite_database_connector
+# class load:
+#     def __init__(self, log_file_path, list_500, base_dir, database_path, database_name):
+#         load_dotenv()
+#         self.__logGenerator = log_generator(log_file_path)
+#         self.__logGenerator.log_config()  # Initialize logging
+#         self.__list_500 = os.getenv(list_500)
+#         self.__base_dir = os.getenv(base_dir)
+#         self.__database_path = os.getenv(database_path)
+#         self.__database_connector = SQLite_database_connector.connect(self.__database_path, database_name) 
+
+#     def load(self):
+#         logging.info("Loading data...")
+#         logging.info(f"Loading data from {self.__list_500} to {self.__base_dir}")
+#         logging.info(f"Loading data completed")
+#         logging.error(f"Data can't be loaded")
+#         logging.error(f"Data is not found")
+
+#     def __load_smp_500(self):
+#         pass
+
+#     def __load_smp_500_data(self):
+#         pass
+
+# load = load("LOG_FILE_PATH", "SMP_500", "RAW_DATA_PATH", "DATABASE_PATH","ok.db")
