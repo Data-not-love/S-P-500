ce4e86b0120e6dd4c1b77ec99842d5daface064b
Lana-DDOS <lana-ddos@LAPTOP-1OIR2NLU.lan>
2025-10-14 16:38:00 +0200

    test shit'

---
+++ Pipeline/test.py
@@ -0,0 +1,21 @@
+# pipeine : how we use raw data for different purposes 
+# some pipelines are used for training the model 
+# some pipelines are used for predicting the future 
+# some pipelines are used for monitoring the model 
+# some pipelines are used for the recommendation system 
+# some pipelines are used for the fraud detection 
+# some pipelines are used for the customer segmentation 
+# some pipelines are used for the churn prediction 
+# some pipelines are used for the risk management 
+# some pipelines are used for the portfolio management 
+# some pipelines are used for the risk management 
+# some pipelines are used for the sales reporting 
+# some pipelines are used for the marketing reporting 
+# some pipelines are used for the finance reporting 
+# some pipelines are used for the operations reporting 
+# some pipelines are used for the human resource reporting 
+# some pipelines are used for the customer service reporting 
+# some pipelines are used for the product management reporting 
+# some pipelines are used for the supply chain reporting 
+# some pipelines are used for the logistics reporting 
+# some pipelines are used for the inventory reporting 
