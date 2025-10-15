44d7a219891c9b0fedda3306f5e03e26507ea8bb
Lana-DDOS <lana-ddos@LAPTOP-1OIR2NLU.lan>
2025-10-15 11:00:46 +0200

    test file

--- Visualization/test.py
+++ Visualization/test.py
@@ -10,7 +10,8 @@ from logGenerator import log_generator
 # find a day that have the most volume for each company
 # each company will have a folder with data visualizations
 # run commnad : python -m "Visualization.test"
-
+# fix it later
+# add more functions later
 class Visualization:
     def __init__(self,log_file_path, raw_data_path, raw_date_file, chart_path, chart_name):
         load_dotenv()
